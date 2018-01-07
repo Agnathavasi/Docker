e5da7d4dac646fc2d78e3c89c3e0481622f5e525
root <root@ip-172-31-30-65.ec2.internal>
2018-01-07 06:53:08 +0000

    Test

--- Hello_Compose/server.js
+++ Hello_Compose/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('------------- Hello world Docker POC ------\n');
+  res.send('------------- Hello world Docker POC --------');
 });
 
 app.listen(PORT, HOST);
